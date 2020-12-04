16d150f48770f6e4691cc65981967684c39a9b77
Zach Thacker <zpthacker@gmail.com>
2020-12-04 00:34:15 -0500

    day 4 code cleanup

--- src/main/kotlin/com/zpthacker/aoc20/day4/Main.kt
+++ src/main/kotlin/com/zpthacker/aoc20/day4/Main.kt
@@ -1,20 +1,26 @@
 package com.zpthacker.aoc20.day4
 
 import com.zpthacker.aoc20.getInput
-import java.lang.NumberFormatException
-import java.lang.RuntimeException
 
 fun main() {
     val passportStrings = getInput("day4").split("\n\n")
+    println("Part 1")
+    val validRequiredFields = passportStrings.count {
+        val fields = findFieldsInPassportString(it)
+        validatePassportRequiredFields(fields)
+    }
+    println("Found $validRequiredFields passports with the required fields")
+    println("Part 2")
     val validCount = passportStrings.count {
         val fields = findFieldsInPassportString(it)
-        validatePassportFields(fields)
+        validatePassportFieldsAndValues(fields)
     }
-    println(validCount)
+    println("Found $validCount passports with valid fields")
 }
 
 fun findFieldsInPassportString(passportString: String): Map<String, String> {
-    return passportString.split(Regex("[\n ]"))
+    return passportString
+        .split(Regex("[\n ]"))
         .mapNotNull {
             val tokens = it.split(":")
             if (tokens.count() != 2) {
@@ -30,9 +36,11 @@ fun findFieldsInPassportString(passportString: String): Map<String, String> {
 val requiredFields = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 val validEyeColors = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
 
-fun validatePassportFields(fields: Map<String, String>): Boolean {
-    val present = fields.keys.containsAll(requiredFields)
-    if (!present) {
+fun validatePassportRequiredFields(fields: Map<String, String>) =
+    fields.keys.containsAll(requiredFields)
+
+fun validatePassportFieldsAndValues(fields: Map<String, String>): Boolean {
+    if (!validatePassportRequiredFields(fields)) {
         return false
     }
     return fields.all { (fieldName, value) ->
@@ -41,7 +49,7 @@ fun validatePassportFields(fields: Map<String, String>): Boolean {
             "iyr" -> value.toInt() in 2010..2020
             "eyr" -> value.toInt() in 2020..2030
             "hgt" -> validateHeight(value)
-            "hcl" -> validateColor(value)
+            "hcl" -> value.matches(Regex("#[a-f0-9]{6}"))
             "ecl" -> value in validEyeColors
             "pid" -> value.matches(Regex("[0-9]{9}"))
             "cid" -> true
@@ -54,16 +62,12 @@ fun validateHeight(height: String): Boolean {
     val units = height.takeLast(2)
     val value = try {
         height.dropLast(2).toInt()
-    } catch(e: NumberFormatException) {
+    } catch (e: NumberFormatException) {
         return false
     }
-    return when(units) {
+    return when (units) {
         "in" -> value in 59..76
         "cm" -> value in 150..193
         else -> false
     }
-}
-
-fun validateColor(color: String): Boolean {
-    return color.matches(Regex("#[a-f0-9]{6}"))
 }
