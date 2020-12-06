21be04cfc539d23cce8c7e8a846da8e6774d6c1e
Zach Thacker <zpthacker@gmail.com>
2020-12-06 00:29:26 -0500

    Day 6 part 2 raw

--- src/main/kotlin/com/zpthacker/aoc20/day6/Main.kt
+++ src/main/kotlin/com/zpthacker/aoc20/day6/Main.kt
@@ -5,13 +5,11 @@ import com.zpthacker.aoc20.getInput
 fun main() {
     val input = getInput("day6")
     val groups = input.split("\n\n")
-    val groupAnswers = groups.map { it.split("\n") }
-    val groupCounts = groupAnswers.map { answers ->
-        answers
-            .fold(mutableSetOf<Char>()) { acc, personAnswers ->
-                acc.addAll(personAnswers.toList())
-                acc
-            }
+    val groupAnswers = groups.map { it.split("\n").filter { person -> person.isNotBlank() } }
+    val groupCounts = groupAnswers.map { people ->
+        people
+            .map { personAnswers -> personAnswers.toSet() }
+            .reduce { acc, set -> acc.intersect(set) }
             .count()
     }
     println(groupCounts.sum())
