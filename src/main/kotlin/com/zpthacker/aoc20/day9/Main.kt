7874ed390db7210b5ee0a446a0db0bd4c9a38917
Zach Thacker <zpthacker@gmail.com>
2020-12-09 00:35:36 -0500

    Day 9 part 2 raw

--- src/main/kotlin/com/zpthacker/aoc20/day9/Main.kt
+++ src/main/kotlin/com/zpthacker/aoc20/day9/Main.kt
@@ -4,17 +4,21 @@ import com.zpthacker.aoc20.getInputLines
 
 fun main() {
     val input = getInputLines("day9").map { it.toLong() }
-    println(detectSum(input))
+    val vulnerableNumber = detectSum(input)
+    println(vulnerableNumber)
+    println(detectRange(input, vulnerableNumber))
 }
 
+val preamble = 25
+
 fun detectSum(input: List<Long>): Long {
-    var index = 25
+    var index = preamble
     while (index <= input.lastIndex) {
         val candidate = input[index]
         var found = false
-        for (i in input) {
+        for (i in input.subList(index - preamble, index)) {
             if (found) { continue }
-            for (j in input) {
+            for (j in input.subList(index - preamble, index)) {
                 if (found) continue
                 if (i + j == candidate) {
                     found = true
@@ -25,6 +29,18 @@ fun detectSum(input: List<Long>): Long {
             return candidate
         } else {
             index += 1
+        }
+    }
+    throw RuntimeException()
+}
+
+fun detectRange(input: List<Long>, targetNumber: Long): Long {
+    for (i in input.indices) {
+        for (j in (i..input.count())) {
+            val numbers = input.subList(i, j)
+            if (numbers.sum() == targetNumber) {
+                return numbers.minOrNull()!! + numbers.maxOrNull()!!
+            }
         }
     }
     throw RuntimeException()
