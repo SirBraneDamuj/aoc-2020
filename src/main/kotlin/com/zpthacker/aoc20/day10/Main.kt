434596268050cef8f4547b10b7ac797590bdcd2e
Zach Thacker <zpthacker@gmail.com>
2020-12-10 01:02:29 -0500

    Day 10 part 2 raw

--- src/main/kotlin/com/zpthacker/aoc20/day10/Main.kt
+++ src/main/kotlin/com/zpthacker/aoc20/day10/Main.kt
@@ -5,6 +5,14 @@ import com.zpthacker.aoc20.getInputLines
 fun main() {
     val lines = getInputLines("day10")
     val joltages = lines.map(String::toInt).sorted()
+    val diffs = countDiffs(joltages)
+    println(diffs.getValue(1) * (diffs.getValue(3) + 1))
+    val amendedList = listOf(0) + joltages + listOf(joltages.maxOrNull()!! + 3)
+    val tree = buildTree(amendedList)
+    println(countPaths(tree, joltages.maxOrNull()!!))
+}
+
+fun countDiffs(joltages: List<Int>): Map<Int, Int> {
     val diffs = mutableMapOf(
         Pair(1, 0),
         Pair(2, 0),
@@ -19,5 +27,33 @@ fun main() {
             diffs.replace(difference, diffs[difference]!!.inc())
         }
     }
-    println(diffs[1]!! * (diffs[3]!! + 1))
+    return diffs
+}
+
+fun buildTree(joltages: List<Int>): Map<Int, List<Int>> {
+    return joltages.fold(mutableMapOf()) { acc, joltage ->
+        acc[joltage] = joltages.filter { it < joltage && (joltage - it) <= 3 }
+        acc
+    }
+}
+
+val cache = mutableMapOf<Int, Long>()
+
+fun countPaths(connections: Map<Int, List<Int>>, joltage: Int): Long {
+    val cacheLookup = cache[joltage]
+    return if (cacheLookup != null) {
+        cacheLookup
+    } else {
+        val result = if (joltage == 0) {
+            1L
+        } else {
+            var sum = 0L
+            for (connection in connections.getValue(joltage)) {
+                sum += countPaths(connections, connection)
+            }
+            sum
+        }
+        cache[joltage] = result
+        result
+    }
 }
