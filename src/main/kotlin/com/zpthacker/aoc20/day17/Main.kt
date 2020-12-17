480c65f0c74550ba7170b0bbf538ca6fea9c3eab
Zach Thacker <zpthacker@gmail.com>
2020-12-17 00:56:04 -0500

    Day 17 cleaned up

--- src/main/kotlin/com/zpthacker/aoc20/day17/Main.kt
+++ src/main/kotlin/com/zpthacker/aoc20/day17/Main.kt
@@ -4,60 +4,82 @@ import com.zpthacker.aoc20.getInputLines
 
 fun main() {
     val lines = getInputLines("day17")
-    val space = mutableMapOf<Vector4, Boolean>()
-    for (i in lines.indices) {
-        val line = lines[i]
-        for (j in line.indices) {
-            val char = line[j]
-            space[Vector4(j, i, 0, 0)] = when (char) {
-                '.' -> false
-                '#' -> true
-                else -> throw RuntimeException()
-            }
-        }
-    }
-    val steps = mutableListOf(space)
-    repeat(6) { _ ->
-        val previous = steps.last()
-        val grid = previous.toMutableMap()
-        val visited = mutableSetOf<Vector4>()
-        for ((coordinates, _) in previous) {
-            checkNode(coordinates, visited, previous, grid)
-            val neighbors = coordinates.neighbors
-            neighbors.forEach { neighbor ->
-                checkNode(neighbor, visited, previous, grid)
-            }
-        }
-        steps.add(grid)
-    }
-    val result = steps.last().count { (_, value) -> value }
-    println(result)
+    val threeDimensionalGrid = SpatialGrid(lines, ::Vector3)
+    val fourDimensionalGrid = SpatialGrid(lines, ::Vector4)
+    println("Part 1: Three dimenstional grid active after 6 simulations")
+    threeDimensionalGrid.simulate(6)
+    println(threeDimensionalGrid.currentActive)
+    println("Part 2: Four dimenstional grid active after 6 simulations")
+    fourDimensionalGrid.simulate(6)
+    println(fourDimensionalGrid.currentActive)
 }
 
-fun checkNode(
-    coordinates: Vector4,
-    visited: MutableSet<Vector4>,
-    previousGrid: Map<Vector4, Boolean>,
-    newGrid: MutableMap<Vector4, Boolean>
-) {
-    if (visited.contains(coordinates)) return
-    else visited.add(coordinates)
 
-    val state = previousGrid[coordinates] ?: false
-    val activeNeighbors = coordinates.neighbors.filter { previousGrid[it] ?: false }.count()
-    val newState = when (state) {
-        true -> activeNeighbors == 2 || activeNeighbors == 3
-        false -> activeNeighbors == 3
+class SpatialGrid<VectorClass : Neighborly<VectorClass>>(
+    zeroSliceRows: List<String>,
+    private val vectorBuilder: (Int, Int) -> VectorClass
+) {
+    private val initialSpace = mutableMapOf<VectorClass, Boolean>().apply {
+        zeroSliceRows.forEachIndexed { i, row ->
+            row.forEachIndexed { j, char ->
+                this[vectorBuilder(j, i)] = when (char) {
+                    '.' -> false
+                    '#' -> true
+                    else -> throw RuntimeException()
+                }
+            }
+        }
     }
-    newGrid[coordinates] = newState
+    private val steps = mutableListOf(initialSpace)
+
+    val currentActive
+        get() = steps.last().count { (_, state) -> state }
+
+    fun simulate(times: Int) {
+        repeat(times) { _ ->
+            val previous = steps.last()
+            val grid = previous.toMutableMap()
+            val visited = mutableSetOf<VectorClass>()
+            for ((coordinates, _) in previous) {
+                checkNode(coordinates, visited, previous, grid)
+                val neighbors = coordinates.neighbors
+                neighbors.forEach { neighbor ->
+                    checkNode(neighbor, visited, previous, grid)
+                }
+            }
+            steps.add(grid)
+        }
+    }
+
+    private fun checkNode(
+        coordinates: VectorClass,
+        visited: MutableSet<VectorClass>,
+        previousGrid: Map<VectorClass, Boolean>,
+        newGrid: MutableMap<VectorClass, Boolean>
+    ) {
+        if (visited.contains(coordinates)) return
+        else visited.add(coordinates)
+
+        val state = previousGrid[coordinates] ?: false
+        val activeNeighbors = coordinates.neighbors.filter { previousGrid[it] ?: false }.count()
+        val newState = when (state) {
+            true -> activeNeighbors == 2 || activeNeighbors == 3
+            false -> activeNeighbors == 3
+        }
+        newGrid[coordinates] = newState
+    }
+}
+
+interface Neighborly<A> {
+    val neighbors: Set<A>
 }
 
 data class Vector3(
     val x: Int,
     val y: Int,
-    val z: Int
-) {
-    val neighbors: Set<Vector3>
+    val z: Int = 0
+) : Neighborly<Vector3> {
+    override val neighbors: Set<Vector3>
         get() {
             val set = mutableSetOf<Vector3>()
             for (xOff in (-1..1)) {
@@ -75,10 +97,10 @@ data class Vector3(
 data class Vector4(
     val x: Int,
     val y: Int,
-    val z: Int,
-    val w: Int
-) {
-    val neighbors: Set<Vector4>
+    val z: Int = 0,
+    val w: Int = 0
+) : Neighborly<Vector4> {
+    override val neighbors: Set<Vector4>
         get() {
             val set = mutableSetOf<Vector4>()
             for (xOff in (-1..1)) {
