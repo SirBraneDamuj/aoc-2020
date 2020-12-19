8b4cde30337e759f3733bf20fd743f7629acd530
Zach Thacker <zpthacker@gmail.com>
2020-12-19 13:12:50 -0500

    Day 18 part 2 raw

--- src/main/kotlin/com/zpthacker/aoc20/day18/Main.kt
+++ src/main/kotlin/com/zpthacker/aoc20/day18/Main.kt
@@ -3,91 +3,87 @@ package com.zpthacker.aoc20.day18
 import com.zpthacker.aoc20.getInputLines
 
 fun main() {
+    println(parseExpression("2 * 3 + (4 * 5)".replace(" ", "")))
     val lines = getInputLines("day18")
     var sum = 0L
     lines.forEach {
-        sum += parseExpression(it)
+        sum += parseExpression(it.replace(" ", ""))
     }
     println(sum)
 }
 
-fun parseExpression(expr: String): Long {
-    var accumulator = 0L
-    var i = 0
-    var numberAccumulator = ""
-    var state = State.LHS
-    var lhs: Long? = null
-    var operator: ((Long, Long) -> Long)? = null
-    while (i <= expr.length) {
-        val token = if (i <= expr.lastIndex) expr[i] else '\n'
-        if (state == State.LHS && token == '\n') break
-        when (token) {
-            '(' -> {
-                var parenCount = 1
-                val subExpr = expr.substring(i + 1).takeWhile {
-                    if (it == '(') {
-                        parenCount++
-                    } else if (it == ')') {
-                        parenCount--
-                    }
-                    parenCount != 0
-                }
-                when (state) {
-                    State.LHS -> {
-                        accumulator = parseExpression(subExpr)
-                        numberAccumulator = accumulator.toString()
-                    }
-                    State.RHS -> {
-                        accumulator = operator!!.invoke(lhs!!, parseExpression(subExpr))
-                        numberAccumulator = accumulator.toString()
-                        state = State.LHS
-                    }
-                    else -> throw RuntimeException()
-                }
-                i += subExpr.length + 1
-            }
-            in ('0'..'9') -> {
-                numberAccumulator += expr[i]
-            }
-            '+' -> {
-                if (state != State.OPERATOR) throw RuntimeException()
-                operator = Long::plus
-            }
-            '*' -> {
-                if (state != State.OPERATOR) throw RuntimeException()
-                operator = Long::times
-            }
-            ' ' -> {
-                when (state) {
-                    State.LHS -> {
-                        lhs = numberAccumulator.toLong()
-                        numberAccumulator = ""
-                        state = State.OPERATOR
-                    }
-                    State.RHS -> {
-                        accumulator = operator!!.invoke(lhs!!, numberAccumulator.toLong())
-                        numberAccumulator = ""
-                        lhs = accumulator
-                        state = State.OPERATOR
-                    }
-                    State.OPERATOR -> {
-                        state = State.RHS
-                    }
-                }
-            }
-            '\n' -> {
-                if (state == State.RHS) {
-                    accumulator = operator!!.invoke(lhs!!, numberAccumulator.toLong())
-                }
-            }
-        }
-        i++
-    }
-    return accumulator
+enum class State {
+    NUMBER,
+    MULTIPLY,
+    ADD,
 }
 
-enum class State {
-    LHS,
-    RHS,
-    OPERATOR
+class ParserStack {
+    val operands: MutableList<Long> = mutableListOf()
+    val operator: (Long, Long) -> Long = Long::times
+    var state: State = State.NUMBER
+
+    fun push(l: Long) = operands.add(l)
+    fun last() = operands.last()
+    fun popAndAdd(augend: Long) {
+        val addend = this.last()
+        operands[operands.lastIndex] = addend + augend
+    }
+
+    val reduction
+        get() = operands.reduce(operator)
 }
+
+fun parseExpression(expr: String): Long {
+    val stacks = mutableListOf(ParserStack())
+    var index = 0
+    while (index < expr.length) {
+        val token = expr[index]
+        val stack = stacks.last()
+        val advance = when (token) {
+            in ('0'..'9') -> {
+                val numberToken = expr.substring(index).takeWhile { it in ('0'..'9') }
+                val number = numberToken.toLong()
+                when (stack.state) {
+                    State.NUMBER, State.MULTIPLY -> {
+                        stacks.last().push(number)
+                    }
+                    State.ADD -> {
+                        stack.popAndAdd(number)
+                    }
+                }
+                numberToken.length
+            }
+            '*' -> {
+                stack.state = State.MULTIPLY
+                1
+            }
+            '+' -> {
+                stack.state = State.ADD
+                1
+            }
+            '(' -> {
+                stacks.add(ParserStack())
+                1
+            }
+            ')' -> {
+                val stackResult = stack.reduction
+                stacks.removeLast()
+                val nextStack = stacks.last()
+                when (nextStack.state) {
+                    State.NUMBER, State.MULTIPLY -> {
+                        nextStack.push(stackResult)
+                    }
+                    State.ADD -> {
+                        nextStack.popAndAdd(stackResult)
+                    }
+                }
+                1
+            }
+            else -> throw RuntimeException()
+        }
+        index += advance
+    }
+    return stacks.single().reduction
+}
+
